b712c653690efb15deb75bf03d5c997ac79627a3
AssanSav <assane.savadogo81@gmail.com>
2019-12-17 12:33:01 -0500

    CRUD for the recipe_controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,37 @@
 class RecipesController < ApplicationController
+  before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @recipes = Recipe.all  
+  end 
+
+  def new 
+    @recipes - Recipe.new 
+  end 
+
+  def create 
+  end 
+
+  def show 
+  end 
+
+  def edit 
+  end 
+
+  def update 
+  end 
+
+  def destroy 
+  end 
+
+  private 
+
+  def find_recipe 
+    @recipe = Recipe.find_by(params[:id])
+  end 
+
+  def recipe_params
+    params.require(:recipe).permit(:name)
+  end 
+
 end

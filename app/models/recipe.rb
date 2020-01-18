5f4e7bbc59d87df1379278bc5c6bbcce951d4a78
AssanSav <assane.savadogo81@gmail.com>
2020-01-18 17:40:50 -0500

    include filterable'

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
+	include Filterable
+
 	has_one_attached :image
 	belongs_to :user
 	belongs_to :category
@@ -12,7 +14,8 @@ class Recipe < ApplicationRecord
 	validates :directions, presence: true 
 	before_validation :name_capitalizer
 
-	scope :query, -> (name) { where("name like ?", "#{name}%")}
+	scope :find_recipes_by_name, -> (name) { where("name like ?", "#{name}%")}
+	scope :find_recipes_by_serving, -> (number_of_persons) { where number_of_persons: number_of_persons }
 
 	accepts_nested_attributes_for :recipe_ingredients, reject_if: :all_blank
 	
@@ -23,6 +26,10 @@ class Recipe < ApplicationRecord
 	def self.desc_listing
 		all.order(created_at: :desc)
 	end
+
+	# def self.servings_query(query)
+	# 	all.where("number_of_persons like ?", "#{query}%")
+	# end
 
 	private 
 

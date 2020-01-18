76b8ee46689959111f7bc331c6448ec78a0b04d3
AssanSav <assane.savadogo81@gmail.com>
2020-01-18 17:37:47 -0500

    concerns module filterable for queries

---
+++ app/models/concerns/filterable.rb
@@ -0,0 +1,15 @@
+module Filterable 
+    extend ActiveSupport::Concern
+
+    module ClassMethods 
+        def filter(filtering_params)
+            binding.pry
+            results = self.where(nil)
+            filtering_params.each do |key, value|
+                results = results.send(key, value) if value.present?
+            end
+            results
+        end
+    end
+
+end

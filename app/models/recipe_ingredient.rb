cc322c4fefcc68cd86173700b524abe0cac6af2f
AssanSav <assane.savadogo81@gmail.com>
2020-01-21 22:23:24 -0500

    custom methods for recipe_ingredient and ingredient on update

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -3,15 +3,12 @@ class RecipeIngredient < ApplicationRecord
     belongs_to :ingredient, optional: true 
 
     validates :amount, presence: true
-    validates :recipe_id, uniqueness: {scope: [:ingredient_id]}
-    accepts_nested_attributes_for :ingredient
+    validates_uniqueness_of :ingredient_id, scope: :recipe_id
     
     def ingredient_attributes=(ingredients_attributes)
-        ingredients_attributes.values.each do |ingr_attributes|
-            if !ingr_attributes.present? 
-                self.ingredient.update(name: ingr_attributes)
-            else
-                self.ingredient = Ingredient.find_or_create_by(name: ingr_attributes)
+        ingredients_attributes.values.each do |ingredient_name|
+            if ingredient_name.present? 
+                self.ingredient = Ingredient.find_or_create_by(name: ingredient_name)
             end
         end
     end

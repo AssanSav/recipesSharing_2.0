bd4da69b2c1764efb70d42b7fc7246eedf53e459
AssanSav <assane.savadogo81@gmail.com>
2020-01-21 22:18:59 -0500

    validate ingredient name uniqueness on update

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,6 +2,6 @@ class Ingredient < ApplicationRecord
     has_many :recipe_ingredients, dependent: :destroy
     has_many :recipes, through: :recipe_ingredients
 
-    validates :name, presence: true, uniqueness: true
-
+    validates :name, presence: true, uniqueness: true, on: :update
+    
 end
